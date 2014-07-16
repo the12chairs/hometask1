99ee98bc09cac4e5af82a3a583661e7f9ab4eb79
Maksim Bondarenko <mbondarenko92@gmail.com>
2014-07-16 17:25:17 +0700

    Coming soon. Let me work until 12

--- task3/csv.php
+++ task3/csv.php
@@ -6,13 +6,73 @@ if (($handle = fopen("categories.csv", "r")) !== FALSE) {
 
 
   // Get file content
-  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    $sp = explode(";", $data[0]); // Here 0 element is id, 1 element is parent id
-    $arr[] = $sp; // 
-   
+  while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+    $arr[] = $data;
+
   }
-  
-  
+    
+
+    // Add extra dimension for tab
+    for($i = 0; $i < count($arr); $i++)
+        $arr[$i]['tab'] = "";
+
+   
+
+
+
+
+
+
+/*
+    function tabs($arr){
+    
+    
+    }
+
+    foreach($arr as $i){
+        
+        if($i[1] == "") echo $i[2]."\n";
+        else{
+            
+        }
+
+
+    }
+    */
+
+    
+
+    function tabs($arr, $end){
+        if ($end == 7) return;
+        else
+            for($i = 0; $i < $end; $i++){
+                if($arr[$i] == "") echo $arr[$i];
+            }
+        tabs($arr, $end+1);
+    }
+
+
+
+    tabs($arr,0);
+/*
+
+    // Fuck, fuck, fuck!
+    foreach($arr as $i){
+        if($i[1] != ""){
+            $i['tab'].= "\t";
+            
+        }
+        
+        echo $i['tab'] . $i[2] . "\n";
+        
+    }
+    
+    //foreach($arr as $i)
+    //    echo "$i[2]$i[1]";
+    //var_dump($arr);
+
+    /*
+    die();
   $flag = 0; // This is bad idea, I know :)
   foreach($arr as $i){
     if($i[1] == ""){
@@ -32,7 +92,7 @@ if (($handle = fopen("categories.csv", "r")) !== FALSE) {
     }
   }
   
-  
+*/
   fclose($handle); // End filework
 }
 
